96f7d0ea8591397f89f98cbb9c496631853ce597
gholamali_rahnavard <gholamali_rahnavard@localhost>
2017-08-11 16:29:12 -0400

    minor fix on select_fasta

--- ppanini/tools/fasta_select.py
+++ ppanini/tools/fasta_select.py
@@ -4,19 +4,24 @@ import sys, re, argparse
 import subprocess
 
 def  get_args():
-    # argument parsing (python argparse)
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="PPANINI selecting sequence of genes form fasta and mapping file",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
     parser.add_argument( 
         '-i', '--input', 
-        help='fasta file', 
+        help='fasta file',
+        required = True, 
         )
     parser.add_argument(
         '-f', '--ok_file',
         help='like grep -f behavior', 
+        required = True,
         )
     parser.add_argument( 
         '-o', '--output', 
-        help='fasta file', 
+        help='fasta file',
+        required = True, 
         )
     args = parser.parse_args()
     return args
