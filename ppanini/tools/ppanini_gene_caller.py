f9d703c7eb92255ada0220f06db4fe71f978af9e
gholamali_rahnavard <gholamali_rahnavard@localhost>
2017-08-15 11:07:37 -0400

    minor fix

--- ppanini/tools/ppanini_gene_caller.py
+++ ppanini/tools/ppanini_gene_caller.py
@@ -119,11 +119,17 @@ def main():
     abundance_file = utilities.abundance(genes_file_gff, alignment_file)
     
     # move the three main output under main output folder from temp files
-    utilities.execute_command("mv", abundance_file, abundance_file, config.output_folder+'/'+os.path.basename(os.path.normpath(abundance_file))+'.txt')
-    utilities.execute_command("mv", genes_file_gff, genes_file_gff, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_gff))+'.gff')
-    utilities.execute_command("mv", genes_file_faa, genes_file_faa, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_faa))+'.faa')
+    mv_args = [abundance_file, config.output_folder+'/'+os.path.basename(os.path.normpath(abundance_file))+'.txt']
+    utilities.execute_command("mv", mv_args, abundance_file, config.output_folder+'/'+os.path.basename(os.path.normpath(abundance_file))+'.txt')
+    
+    mv_args = [genes_file_gff, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_gff))+'.gff']
+    utilities.execute_command("mv", mv_args, genes_file_gff, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_gff))+'.gff')
+    
+    mv_args = [genes_file_faa, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_faa))+'.faa']
+    utilities.execute_command("mv", mv_args, genes_file_faa, config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_faa))+'.faa')
     print ("Three main output files for ppanini_press are written in: \n%s\n%s\n%s")% (config.output_folder+'/'+os.path.basename(os.path.normpath(abundance_file))+'.txt',
-                                                config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_gff))+'.gff', 
-                                                config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_faa))+'.faa')
+                                            config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_gff))+'.gff', 
+                                            config.output_folder+'/'+os.path.basename(os.path.normpath(genes_file_faa))+'.faa')
+    
 if __name__ == '__main__':
 	main()
