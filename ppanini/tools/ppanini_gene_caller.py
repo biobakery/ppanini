6d41a0ff0aaf538df5fd5caa8e3cf47768486aa8
gholamali_rahnavard <gholamali_rahnavard@localhost>
2017-09-21 16:05:01 -0400

    minor fix

--- ppanini/tools/ppanini_gene_caller.py
+++ ppanini/tools/ppanini_gene_caller.py
@@ -146,7 +146,7 @@ def main():
     # Run diamond
     alignment_file = utilities.diamond_alignment(genes_file_faa, args.uniref )
     
-    
+    config.temp_dir = temp_dir
     # Infer abundance for sufficient hits to  uniref90 and no_hits
     hits_map, no_hits_map = utilities.Infer_aligmnets(alignment_file, config.temp_dir)
     
