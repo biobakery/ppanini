de99eb4feab963b40b019f5cea5a8e1ed2328064
gholamali_rahnavard <gholamali_rahnavard@localhost>
2017-09-25 18:40:28 -0400

    minor fix

--- ppanini/tools/ppanini_gene_caller.py
+++ ppanini/tools/ppanini_gene_caller.py
@@ -117,8 +117,10 @@ def main():
     else:
         new_contig_file = args.contig 
         # check if there is already a prodigal output
-        if not os.path.isfile(config.output_folder+"/prodigal.gff"): # if there is no prodigal output   
+        if not os.path.isfile(config.output_folder+"/prodigal.gff") or not os.path.isfile(config.output_folder+"/prodigal.faa"): # if there is no prodigal output   
             # gene call using prodigal
+            config.temp_dir = temp_dir+'/prodigal_output/'
+            utilities.make_directory(config.temp_dir)
             genes_file_gff, genes_file_fna, genes_file_faa = utilities.genecall(new_contig_file)
         else:
             genes_file_gff = config.output_folder+ '/prodigal.gff'
@@ -171,14 +173,13 @@ def main():
             shutil.move(no_hits_map, config.output_folder+'/no_hits/'+ config.file_basename+ '_no_hits.txt')
             shutil.move(hits_map, config.output_folder+'/hits/'+ config.file_basename+ '_hits.txt')
             shutil.move(genes_file_gff, config.output_folder+'/prodigal.gff')
-            #shutil.move(genes_file_faa, config.output_folder+'/prodigal.faa')
+            shutil.move(genes_file_faa, config.output_folder+'/prodigal.faa')
             print ("Main output files for ppanini_press are written in: \n%s\n%s\n%s")% (config.output_folder+'/'+os.path.basename(os.path.normpath(abundance_file)),
                                                 config.output_folder+'/prodigal.gff', 
                                                 config.output_folder+'/no_hits/'+ config.file_basename+ '_no_hits.faa',
                                                 config.output_folder+'/no_hits/'+ config.file_basename+ '_no_hits.txt',
                                                 config.output_folder+'/hits/'+ config.file_basename+ '_hits.faa',
-                                                config.output_folder+'/hits/'+ config.file_basename+ '_hits.txt'
-                                                )
+                                                config.output_folder+'/hits/'+ config.file_basename+ '_hits.txt')
         else:
             shutil.move(no_hits_genes_faa, config.output_folder+'/no_hits/'+config.file_basename+'_no_hits.faa')
             shutil.move(hits_genes_faa, config.output_folder+'/hits/'+config.file_basename+'_hits.faa')
